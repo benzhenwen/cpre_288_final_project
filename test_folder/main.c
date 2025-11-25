377b9e288f32d18d3bac2b29c710dea617d198b6
benzhenwen <benzhenwen@gmail.com>
2025-11-24 22:07:46 -0600

    first, setup

---
+++ test_folder/main.c
@@ -0,0 +1,56 @@
+#include <math.h>
+#include <stdio.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+typedef struct object_positional {
+    float x; // x pos in mm
+    float y; // y pos in mm
+    float radius; // radius of the object in mm;
+    char type; // 0 is short object, 1 is tall object, 2 is hole (black), 3 is edge (white)
+} object_positional;
+
+void ur_send_line(char * str) {
+    printf("uart_line: %s\n", str);
+}
+
+#include "main_scan_data.h"
+#include "main_pathfinding.h"
+
+void add_object(object_positional object) {
+    object_map[object_map_c] = object;
+    object_map_c++;
+}
+    
+// position stuff
+float pos_x = 0;
+float pos_y = 0;
+float pos_r = 0;
+
+float get_pos_x() { return pos_x; }
+float get_pos_y() { return pos_y; }
+float get_pos_r() { return pos_r; }
+
+void move_to(float x, float y) {
+    pos_x = x;
+    pos_y = y;
+}
+
+void print_objs_as_desmos() {
+    int i;
+    for (i = 0; i < object_map_c; i++) {
+        object_positional o = object_map[i];
+        printf("\\left(x-%.2f\\right)^{2}+\\left(y-%.2f\\right)^{2}=%.2f^{2}\n", o.x, o.y, o.radius);
+    }
+}
+
+
+
+int main() {
+    printf("meow\n");
+
+    add_object((object_positional) {500, 0, 40, 1});
+    print_objs_as_desmos();
+}
---
+++ test_folder/main_scan_data.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+
+
+
+// only allow objects up to x cm away
+#define SCAN_MAX_DISTANCE 80
+
+// object map (xy based)
+object_positional object_map[64];
+int object_map_c;
+
+
